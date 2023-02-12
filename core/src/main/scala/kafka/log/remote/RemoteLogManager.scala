0a34471bba38f780885a68e0cb535069aafc5c10
Satish Duggana <satishd@apache.org>
2023-02-12 17:19:49 +0530

    KAFKA-14522 Rewrite and Move of RemoteIndexCache to storage module. Cleanedup index file suffix usages and other minor cleaups

--- core/src/main/scala/kafka/log/remote/RemoteLogManager.scala
+++ core/src/main/scala/kafka/log/remote/RemoteLogManager.scala
@@ -26,6 +26,7 @@ import org.apache.kafka.common.utils.{ChildFirstClassLoader, Utils}
 import org.apache.kafka.server.log.remote.metadata.storage.ClassLoaderAwareRemoteLogMetadataManager
 import org.apache.kafka.server.log.remote.storage._
 import org.apache.kafka.storage.internals.epoch.LeaderEpochFileCache
+import org.apache.kafka.storage.internals.log.RemoteIndexCache
 
 import java.io.{Closeable, InputStream}
 import java.security.{AccessController, PrivilegedAction}
@@ -55,7 +56,7 @@ class RemoteLogManager(rlmConfig: RemoteLogManagerConfig,
   private val remoteLogStorageManager: RemoteStorageManager = createRemoteStorageManager()
   private val remoteLogMetadataManager: RemoteLogMetadataManager = createRemoteLogMetadataManager()
 
-  private val indexCache = new RemoteIndexCache(remoteStorageManager = remoteLogStorageManager, logDir = logDir)
+  private val indexCache = new RemoteIndexCache(remoteLogStorageManager, logDir)
 
   private var closed = false
 
